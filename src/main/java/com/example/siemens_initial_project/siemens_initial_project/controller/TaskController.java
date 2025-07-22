55ac6cd2d43e3bdde60a47b3907fef9ec6a0a848
Shefoo <shefoolinux@gmail.com>
2025-07-22 17:34:01 +0300

    Inizialize the controller with some edits in other layers because of some tests it in postman

---
+++ src/main/java/com/example/siemens_initial_project/siemens_initial_project/controller/TaskController.java
@@ -0,0 +1,53 @@
+package com.example.siemens_initial_project.siemens_initial_project.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.siemens_initial_project.siemens_initial_project.dto.TaskDto;
+import com.example.siemens_initial_project.siemens_initial_project.services.TaskService;
+
+import lombok.RequiredArgsConstructor;
+
+
+
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping
+      public ResponseEntity<List<TaskDto>> getAllTasks() {
+        List<TaskDto> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDto> createTask(@RequestBody TaskDto taskDto) {
+        TaskDto createdTask = taskService.createTask(taskDto);
+        return ResponseEntity.ok(createdTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDto> updateTask(@PathVariable Long id, @RequestBody TaskDto taskDto) {
+        TaskDto updatedTask = taskService.updateTask(id, taskDto);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+      public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/example/siemens_initial_project/siemens_initial_project/mapper/TaskMapper.java
+++ src/main/java/com/example/siemens_initial_project/siemens_initial_project/mapper/TaskMapper.java
@@ -3,6 +3,7 @@ package com.example.siemens_initial_project.siemens_initial_project.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.example.siemens_initial_project.siemens_initial_project.dto.TaskDto;
@@ -17,5 +18,6 @@ public interface TaskMapper {
 
     List<TaskDto> toDtoList(List<Task> tasks);
 
+    @Mapping(target = "id", ignore = true)
     Task update(TaskDto taskDto, @MappingTarget Task task);
 }
